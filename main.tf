bdfe887525221c05447c6c996b3feb2dc8e568ad
vasanthshanmugam <32473491+vasanthshanmugam@users.noreply.github.com>
2024-09-28 19:08:38 +0100

    Updated terraform files

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
-resource "azurerm_resource_group" "resourcegroups" {
-    name        = var.ResourceGroup
-    location    = var.Location
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
-variable "Location" {
-    type        = string
-    default     = "WestEurope"  
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
 }
 
-variable "ResourceGroup" {
-    type        = string
-    default     = "rg-connectivity-network-001"   
+variable "location" {
+  type        = string
+  description = "Azure region for the resource group"
+  default     = "eastus"
 }
