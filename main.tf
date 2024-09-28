23974dbcccb3276acc89024516e95f71d834071f
vasanthshanmugam <32473491+vasanthshanmugam@users.noreply.github.com>
2024-09-28 17:25:21 +0100

    add terraform files

--- main.tf
+++ main.tf
@@ -1,26 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.40.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  
-  # Specify your subscription ID here
- # subscription_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"  # Replace with your actual subscription ID
-}
-
-resource "azurerm_resource_group" "example_rg" {
-  name     = "example-resource-group"
-  location = "East US"
-
-  tags = {
-    Environment = "Production"
-    Owner       = "littleslaw"
-    Department  = "IT"
-  }
+resource "azurerm_resource_group" "resourcegroups" {
+    name        = var.ResourceGroup
+    location    = var.Location
 }
